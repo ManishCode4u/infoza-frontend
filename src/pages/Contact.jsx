46cbdddc795f11c0f7b78c388bb3acca9448a2ed
Manish kumar <manish12643@gmail.com>
2026-02-03 19:16:40 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://buildoracraft.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
